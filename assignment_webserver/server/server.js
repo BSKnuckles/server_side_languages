7760a05e22c1261c9e55598785224b51fccc7d70
Michael McNees <mcnees.michael@gmail.com>
2021-04-18 21:56:49 -0400

    Completed web server assignment

---
+++ assignment_webserver/server/server.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const http = require('http');
+const path = require('path');
+
+const host = '0.0.0.0'
+const port = 8080
+const client = '../client'
+
+http.createServer(function (req, res) {
+
+    let pathName = path.parse(req.url).name
+    let content = client + req.url
+    let ext = path.extname(content)
+
+    fs.readFile(content, function (err, data) {
+
+        res.writeHead(200) // header()
+
+        if (ext == '.html') {
+            res.write(
+                `<script>const routes = ['index', 'about', 'products', 'store']; const activePage = "${pathName}";</script>`)
+        }
+
+        res.end(data)
+
+    })
+
+}).listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`)
+})
