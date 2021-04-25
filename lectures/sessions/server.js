ac84238c226f5a5ecb33294e95d488932d18097d
Michael McNees <mcnees.michael@gmail.com>
2021-04-25 15:13:18 -0400

    Completed login assignment

--- lectures/sessions/server.js
+++ lectures/sessions/server.js
@@ -17,24 +17,68 @@ const router = express.Router()
 let app = express()
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(
+	bodyParser.urlencoded({
+		extended: true,
+	}),
+)
 app.set('view engine', 'ejs')
 app.engine('ejs', require('ejs').__express)
 
+const session = require('express-session')
+app.use(
+	session({
+		secret: 'secret',
+		saveUninitialized: true,
+		resave: true,
+	}),
+)
+let sess
+
 router.get('/', function (req, res) {
-	res.render('index', { pagename: 'home' }) // /views/index.ejs
+	sess = req.session
+	res.render('index', {
+		pagename: 'home',
+		sess: sess,
+	}) // /views/index.ejs
 })
 
 router.get('/about', function (req, res) {
-	res.render('about', { pagename: 'about' }) // /views/about.ejs
+	sess = req.session
+	res.render('about', {
+		pagename: 'about',
+		sess: sess,
+	}) // /views/about.ejs
+})
+
+router.get('/profile', function (req, res) {
+	sess = req.session
+	if (typeof (sess) == 'undefined' || sess.loggedin != true) {
+		let errors = ['Not an authenticated user']
+		res.render('index', {
+			pagename: 'home',
+			errors: errors,
+		})
+	} else {
+		res.render('profile', {
+			pagename: 'profile',
+			sess: sess,
+		})
+	}
+})
+
+router.get('/logout', function (req, res) {
+	sess = req.session
+	sess.destroy(function (err) {
+		res.redirect('/')
+	})
 })
 
 router.post('/login', function (req, res) {
-	console.log(req.body)
-
+	sess = req.session
 	let errors = []
 
-	// Error checks
+	// Form validation checks
 	if (req.body.email == '') {
 		errors.push('Email is a required field.')
 	}
@@ -48,17 +92,33 @@ router.post('/login', function (req, res) {
 	) {
 		errors.push('Email is not in a valid format.')
 	}
-	if (!/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(req.body.password)) {
+	if (!/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/.test(req.body.password)) {
 		errors.push(
 			'Password is invalid. Requires at least 8 characters, one letter and one number.',
 		)
 	}
 
-	console.log(errors)
-
-	res.render('index', { pagename: 'home', errors: errors })
-
-	// res.redirect('/')
+	// Redirect to index with errors in form validation fails
+	if (errors.length > 0) {
+		res.render('index', {
+			pagename: 'home',
+			errors: errors,
+		})
+	} else {
+		if (req.body.email != 'mike@aol.com' && req.body.password != 'abc123') {
+			sess.loggedin = false
+			res.render('index', {
+				pagename: 'home',
+				errors: ['Invalid credentials.'],
+			})
+		} else {
+			sess.loggedin = true
+			res.render('profile', {
+				pagename: 'profile',
+				sess: sess,
+			})
+		}
+	}
 })
 
 app.use(express.static('public'))
